efab7193fc71012f864adf9d86ecf4745cdfd574
lisekilis <101191226+lisekilis@users.noreply.github.com>
2026-03-18 23:29:53 +0100

    test

--- src/index.ts
+++ src/index.ts
@@ -20,7 +20,11 @@ export default {
 		const method = request.method;
 		const pathParts = path.split('/').filter(Boolean);
 
-		if (pathParts[0] === 'interactions' && method === 'POST') return interactionHandler(request, env, ctx);
+		if (pathParts[0] === 'interactions' && method === 'POST')
+			return interactionHandler(request, env, ctx).then((res) => {
+				console.log('Interaction Handled', res);
+				return res;
+			});
 		if (pathParts[0] === 'link' && method === 'GET') return linkHandler(request, env);
 
 		return new Response('Not Found', { status: 404 });
