9e3e5e8761b6f08cfb51c3091fc48a685dc46efb
lisekilis <101191226+lisekilis@users.noreply.github.com>
2026-04-21 14:37:38 +0200

    better user patching

--- src/helpers/user.ts
+++ src/helpers/user.ts
@@ -1,26 +1,17 @@
 import { UserData, XboxUserData } from '../types';
 
 export async function patchUser(env: Env, discordID: string, userData: Partial<UserData>): Promise<void> {
-	const existingUser = JSON.parse((await env.users.get(discordID)) || `{}`);
+	const existingUser: UserData = JSON.parse((await env.users.get(discordID)) || `{}`);
+	let newUser: UserData = { ...existingUser, ...userData };
 	if (existingUser) {
-		if (userData.xboxAccounts && existingUser.xboxAccounts) {
-			// Merge Xbox accounts by xboxUserHash
-			const mergedXboxAccounts = [...existingUser.xboxAccounts];
-			for (const newAccount of userData.xboxAccounts) {
-				const index = mergedXboxAccounts.findIndex((acc: XboxUserData) => acc.xboxUserHash === newAccount.xboxUserHash);
-				if (index !== -1) {
-					// Replace Minecraft account data if Xbox account already exists
-					mergedXboxAccounts[index].minecraftAccount = newAccount.minecraftAccount;
-					// If there are other fields in XboxUserData in the future, they should also be merged here
-				} else {
-					mergedXboxAccounts.push(newAccount);
-				}
-			}
-			userData.xboxAccounts = mergedXboxAccounts;
-			delete existingUser.xboxAccounts;
+		if (userData.xboxAccounts) {
+			const existingXboxAccounts = existingUser.xboxAccounts || [];
+			const newXboxAccounts = userData.xboxAccounts.filter(
+				(newAcc) => !existingXboxAccounts.some((existingAcc) => existingAcc.xboxUserHash === newAcc.xboxUserHash),
+			);
+			newUser.xboxAccounts = [...existingXboxAccounts, ...newXboxAccounts];
 		}
 	}
-	const newUser = { ...existingUser, ...userData };
 
 	await env.users.put(discordID, JSON.stringify(newUser));
 }
